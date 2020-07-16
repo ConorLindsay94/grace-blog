46b2a51aeadcd3b56612a7b69b1eb60ba1320f02
Conor Lindsay <conor.lindsay@ii.co.uk>
2020-07-16 21:18:29 +0100

    Removal of query

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -9,7 +9,6 @@ import ArticlePreview from '../components/article-preview'
 class BlogIndex extends React.Component {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
 
     return (
       <Layout location={this.props.location}>
@@ -18,15 +17,6 @@ class BlogIndex extends React.Component {
           <div className={styles.hero}>Blog</div>
           <div className="wrapper">
             <h2 className="section-headline">Recent articles</h2>
-            <ul className="article-list">
-              {posts.map(({ node }) => {
-                return (
-                  <li key={node.slug}>
-                    <ArticlePreview article={node} />
-                  </li>
-                )
-              })}
-            </ul>
           </div>
         </div>
       </Layout>
@@ -34,24 +24,4 @@ class BlogIndex extends React.Component {
   }
 }
 
-export default BlogIndex
-
-export const pageQuery = graphql`
-  query BlogIndexQuery {
-    allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
-      edges {
-        node {
-          title
-          slug
-          publishDate(formatString: "MMMM Do, YYYY")
-          tags
-          description {
-            childMarkdownRemark {
-              html
-            }
-          }
-        }
-      }
-    }
-  }
-`
+export default BlogIndex;
